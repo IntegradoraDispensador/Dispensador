ddb4f064307fa5defe0328b05a86bff0634cbb03
GerardoDM <geradelgado22@hotmail.com>
2020-04-08 18:30:46 -0500

    avance android retrofit register

--- Android/app/src/main/java/com/example/dispensadorandroid/MyApp.java
+++ Android/app/src/main/java/com/example/dispensadorandroid/MyApp.java
@@ -2,7 +2,22 @@ package com.example.dispensadorandroid;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+
+import leakcanary.LeakCanary;
+
 public class MyApp extends Application {
 
 
+    @Override
+    public void onCreate(){
+
+        super.onCreate();
+
+        Stetho.initializeWithDefaults(this);
+
+
+
+
+    }
 }
--- Android/app/src/main/java/com/example/dispensadorandroid/register.java
+++ Android/app/src/main/java/com/example/dispensadorandroid/register.java
@@ -3,12 +3,23 @@ package com.example.dispensadorandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
+
+import com.example.dispensadorandroid.entities.AccessToken;
+import com.example.dispensadorandroid.network.ApiService;
+import com.example.dispensadorandroid.network.RetrofitBuilder;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class register extends AppCompatActivity {
+
+    private  static  final String TAG = "Register";
 
     @BindView(R.id.input_name)
     EditText inputname;
@@ -19,6 +30,9 @@ public class register extends AppCompatActivity {
     @BindView(R.id.input_password)
     EditText inputpassword;
 
+    ApiService service;
+    Call<AccessToken> call;
+
 
 
 
@@ -28,5 +42,51 @@ public class register extends AppCompatActivity {
         setContentView(R.layout.register);
 
         ButterKnife.bind(this);
+
+        service = RetrofitBuilder.CreateService(ApiService.class);
+    }
+
+    @OnClick(R.id.btn_signup)
+         void register(){
+
+            String name = inputname.getText().toString();
+            String email = inputemail.getText().toString();
+            String password = inputpassword.getText().toString();
+
+            call = service.register(name, email, password);
+            call.enqueue(new Callback<AccessToken>() {
+                @Override
+                public void onResponse(Call<AccessToken> call, Response<AccessToken> response) {
+
+
+                    Log.w(TAG, "on response: " + response);
+                    if (response.isSuccessful()){
+
+                    }
+
+                    else{
+
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<AccessToken> call, Throwable t) {
+
+                    Log.w(TAG, "on failure: " + t.getMessage());
+
+                }
+            });
+        }
+
+
+    @Override
+    protected void onDestroy(){
+
+        super.onDestroy();
+        if (call != null) {
+            call.cancel();
+            call = null;
+        }
+
     }
 }
