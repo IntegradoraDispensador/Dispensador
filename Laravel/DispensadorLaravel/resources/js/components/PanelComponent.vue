281868ab0698ccbdc2b62830371b7e0167e2c2b8
GerardoDM <geradelgado22@hotmail.com>
2020-04-16 22:30:23 -0500

    npm install y guzzle

---
+++ Laravel/DispensadorLaravel/resources/js/components/PanelComponent.vue
@@ -0,0 +1,19 @@
+<template>
+        <div class="container">
+            <hr>
+            <br>
+            <button type="button" class="btn btn-primary btn-lg">Abrir croquetas</button>
+            <button type="button" class="btn btn-secondary btn-lg">Cerrar croquetas</button>
+            <button type="button" class="btn btn-primary btn-lg">Abrir agua</button>
+            <button type="button" class="btn btn-secondary btn-lg">Cerrar agua</button>
+            <br>
+            <hr>
+      
+        </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ Laravel/DispensadorLaravel/resources/views/panel.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <title>Panel</title>
+</head>
+<body>
+    <div id="app">
+        <panel-component></panel-component>
+    </div>
+</body>
+<script src="{{ asset('js/app.js') }}"></script>
+
+</html>
